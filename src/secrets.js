dd672fede0114620fa20b1f11b41ef909144d676
paras luvani <paras.luvani@sequence.sg>
2024-11-25 20:22:35 +0530

    [dev] :: secret file export each value

--- src/secrets.js
+++ src/secrets.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
-module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET,
-  PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
-};
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const PORT = process.env.PORT;
+export const DATABASE_URL = process.env.DATABASE_URL;
