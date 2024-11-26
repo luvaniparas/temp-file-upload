f53575e350bff4a11ae94ac3a8f0871f82c58074
paras luvani <paras.luvani@sequence.sg>
2024-11-26 15:56:46 +0530

    [dev] :: utils code

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -4,32 +4,50 @@ import { INVALID_INPUT } from "../utils/constant.js";
 /**
  * Middleware to validate request payload against a given Zod schema.
  * @param {import("zod").ZodSchema} schema - The Zod schema to validate against.
+ * @param {boolean} isParamBased - Optional flag to indicate if validation should be for params or body.
  * @returns {Function} Express middleware function.
  */
-const validateSchema = (schema) => (req, res, next) => {
-  const { logger } = req;
+const validateSchema =
+  (schema, isParamBased = false) =>
+  (req, res, next) => {
+    const { logger } = req;
 
-  logger.info("[ValidationMiddleware] :: Validating request payload");
+    logger.info("[ValidationMiddleware] :: Validating request payload");
 
-  const result = schema.safeParse(req.body);
+    let dataToValidate;
 
-  if (!result.success) {
-    const errorMessages = result.error.errors
-      .map((err) => `${err.message} (Path: ${err.path.join(".")})`)
-      .join(", ");
+    // Check if the validation is based on params or body
+    if (isParamBased) {
+      // Validate parameters (e.g., for GET / DELETE requests)
+      dataToValidate = req.params;
+    } else {
+      // Validate body (e.g., for POST / PUT requests)
+      dataToValidate = req.body;
+    }
 
-    logger.error("[ValidationMiddleware] :: Validation errors:", errorMessages);
+    // Perform validation
+    const result = schema.safeParse(dataToValidate);
 
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      message: INVALID_INPUT,
-      errors: errorMessages,
-    });
-  }
+    if (!result.success) {
+      const errorMessages = result.error.errors
+        .map((err) => `${err.message} (Path: ${err.path.join(".")})`)
+        .join(", ");
 
-  // Attach validated data to the request object for downstream use
-  req.validatedData = result.data;
-  logger.info("[ValidationMiddleware] :: Validation successful");
-  next();
-};
+      logger.error(
+        "[ValidationMiddleware] :: Validation errors:",
+        errorMessages
+      );
+
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: INVALID_INPUT,
+        errors: errorMessages,
+      });
+    }
+
+    // Attach validated data to the request object for downstream use
+    req.validatedData = result.data;
+    logger.info("[ValidationMiddleware] :: Validation successful");
+    next();
+  };
 
 export default validateSchema;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,3 +4,8 @@ export const USER_NOT_FOUND_MESSAGE = "User not found";
 export const INVALID_PASSWORD_MESSAGE = "Invalid password";
 export const FAILED_TO_CREATE_FILE =
   "Failed to create file. Please try again later.";
+
+export const FILE_STATUS = {
+  ACTIVE: "active",
+  EXPIRED: "expired",
+};
