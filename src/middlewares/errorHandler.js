6f821ee5e5f932fd23377071038cfc50553e85f2
paras luvani <paras.luvani@sequence.sg>
2024-11-26 21:52:02 +0530

    [dev] :: code formatting

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) => {
   const { logger } = req;
 
   // Log the error
-  // logger.error("[ErrorHandler] :: An error occurred:", err);
+  logger.error("[ErrorHandler] :: An error occurred:", err);
 
   // Determine the status code
   const statusCode = err?.status || StatusCodes.INTERNAL_SERVER_ERROR;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import authController from "../controllers/authController.js";
 import validateSchema from "../middlewares/validateSchema.js";
+
 import { createUserSchema, loginSchema } from "../schema/users.js";
 
 const { register, login, logout } = authController;
@@ -8,7 +9,9 @@ const { register, login, logout } = authController;
 const authRouter = Router();
 
 authRouter.post("/sing-up", validateSchema(createUserSchema), register);
+
 authRouter.get("/login", validateSchema(loginSchema), login);
+
 authRouter.get("/logout", logout);
 
 export default authRouter;
